58f425130ca8e063fc258a72c0f832afbf4a7da8
Brian Lobo <brianlobo.code@gmail.com>
2019-03-27 21:41:10 -0400

    JS logic is all set up... just need to style now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-alert('hello');
+// Check off items by clicking
+$('ul').on('click', 'li', function () {
+    $(this).toggleClass('clicked');
+});
+
+// Deleting items when clicking 'x'
+$('ul').on('click', '.delete', function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function (event) {
+    // If user hits enter
+    if (event.which === 13) {
+        // saves what user typed into item
+        let item = $(this).val();
+        // sets input to blank
+        $(this).val('');
+        // creats li to add to ul
+        $('ul').append('<li><span class="delete">X</span> ' + item + '</li>');
+    }
+});
